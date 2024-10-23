817558125477bfa084673de3568d26c1bc638e48
DVODeveloper <vyurkovd@yandex.ru>
2024-10-23 07:55:25 +0300

    hw - 3_1_3 -v4 delete DAO

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -4,61 +4,113 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserDetailsService, UserService {
 
-    private final UserDao userDao;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDao userDao) {
-        this.userDao = userDao;
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userDao.loadUserByUsername(username);
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
     }
 
     @Override
     public User findUserById(Long userId) {
-        return userDao.findUserById(userId);
+        Optional<User> userFromDb = userRepository.findById(userId);
+        return userFromDb.orElse(new User());
     }
 
     @Override
     public User findUserByUsername(String username) {
-        return userDao.findUserByUsername(username);
+        return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> allUsers() {
-        return userDao.allUsers();
+        return userRepository.findAll();
     }
 
     @Override
     public void addNewUser(String username, String password, String confirmPassword, String roleName) {
-        userDao.addNewUser(username, password, confirmPassword, roleName);
+        if (!password.equals(confirmPassword)) {
+            throw new IllegalArgumentException("Пароли не совпадают");
+        }
+
+        if (userRepository.findByUsername(username) != null) {
+            throw new IllegalArgumentException("Пользователь с таким именем уже существует");
+        }
+
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setPassword(bCryptPasswordEncoder.encode(password));
+
+        Role userRole = roleRepository.findByName(roleName);
+        if (userRole == null) {
+            throw new IllegalArgumentException("Роль не найдена");
+        }
+
+        newUser.setRoles(Collections.singleton(userRole));
+
+        userRepository.save(newUser);
     }
 
     @Override
     public void deleteUser(Long userId) {
-        userDao.deleteUser(userId);
+        User user = userRepository.findById(userId).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        user.getRoles().clear();
+        userRepository.save(user);
+        userRepository.delete(user);
     }
 
     @Override
     public void updateUser(User userForm) {
-        userDao.updateUser(userForm);
+        User user = userRepository.findById(userForm.getId())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        user.setUsername(userForm.getUsername());
+
+        if (userForm.getPassword() != null && !userForm.getPassword().trim().isEmpty()) {
+            user.setPassword(bCryptPasswordEncoder.encode(userForm.getPassword()));
+        }
+
+        if (userForm.getRoles() != null && !userForm.getRoles().isEmpty()) {
+            Set<Role> roles = new HashSet<>();
+            for (Role role : userForm.getRoles()) {
+                Role dbRole = roleRepository.findById(role.getId())
+                        .orElseThrow(() -> new IllegalArgumentException("Role not found"));
+                roles.add(dbRole);
+            }
+            user.setRoles(roles);
+        }
+        userRepository.save(user);
     }
 
     @Override
     public List<Role> findAllRoles() {
-        return userDao.findAllRoles();
+        return roleRepository.findAll();
     }
 }
